adee80e67ab0e754a1376f517dec90b70a4c050c
Oleg Grynkevych <oleg252.og@gmail.com>
2023-09-01 12:29:13 +0300

    Added EthicsCode Field

---
+++ src/api/ethics-code/controllers/ethics-code.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ethics-code controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ethics-code.ethics-code');
---
+++ src/api/ethics-code/routes/ethics-code.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ethics-code router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ethics-code.ethics-code');
